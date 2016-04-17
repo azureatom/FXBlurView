00a284cd18e516ae5f84ea64f09e91a849ebbb2f
azureatom <xue.lei@live.com>
2016-04-17 22:34:10 +0800

    modify demo.

--- Examples/AnimatedBlurExample/FXBlurViewExample/ViewController.m
+++ Examples/AnimatedBlurExample/FXBlurViewExample/ViewController.m
@@ -20,18 +20,35 @@
 
 @implementation ViewController
 
+-(void)viewDidLoad{
+    [super viewDidLoad];
+    
+    self.blurView.dynamic = NO;
+    
+    //default blurRadius is 40 if not specified. We set 0 here. 0 doesn't mean no blur, but blur a little.
+    self.blurView.blurRadius = 0;
+    self.blurView.alpha = 0; //even blurRadius 0 toggle blur effect a little. To show without blur, should make it transparent.
+    
+    //tintColor prefer [UIColor clearColor] or [UIColor blackColor].
+    self.blurView.tintColor = [UIColor clearColor];
+//    self.blurView.blurRadius = 0;
+}
+
 - (IBAction)toggleBlur
 {
     if (self.blurView.blurRadius < 5)
     {
         [UIView animateWithDuration:0.5 animations:^{
             self.blurView.blurRadius = 40;
+            self.blurView.alpha = 1;
         }];
     }
     else
     {
         [UIView animateWithDuration:0.5 animations:^{
             self.blurView.blurRadius = 0;
+            self.blurView.alpha = 0;
+            self.blurView.blurEnabled = NO;
         }];
     }
 }
